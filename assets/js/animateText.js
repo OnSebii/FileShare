f837746c5aec147e0099fd4255dcae63cf689620
OnSebii <lang.sebastian.006@gmail.com>
2021-03-02 10:40:01 +0100

    created first index.html file

---
+++ assets/js/animateText.js
@@ -0,0 +1,16 @@
+//https://github.com/jahdaic/Glitch-Text
+//$('.glitch').glitch({ speed: 280, duration: 5000 });
+
+/* 
+var typed = new Typed('#subtitle', {
+  strings: ['Share your files without an login'],
+  typeSpeed: 90,
+  startDelay: 150,
+  showCursor: true,
+  cursorChar: '|',
+  autoInsertCss: true,
+  onComplete: (self) => {
+    self.cursor.hidden = true;
+  },
+});
+*/
