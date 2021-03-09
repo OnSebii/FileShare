f792eb761d94cfa90ab47deadc91ead44557cc28
domi555 <66876360+domi555@users.noreply.github.com>
2021-03-09 10:11:08 +0100

    DB Update
    ...

--- database/commands.sql
+++ database/commands.sql
@@ -43,3 +43,67 @@ WHERE email = 'lang.s03@htlwienwest.at';
 DELETE
 FROM files
 WHERE id = 1;
+
+--- --- USEFUL QUERIES --- --- --- --- --- --- --- --- --- --- # User
+
+-- Login Process
+SELECT *
+FROM users
+WHERE email = 'lang.s03@htlwienwest.at'
+  AND password = 'hash';
+-- Login GET User Data
+SELECT id, name, path, synonym_path, upload_date
+FROM users
+         JOIN user_data USING (email)
+         JOIN files ON id = data_id
+WHERE email = 'lang.s03@htlwienwest.at';
+
+-- Verification Check
+SELECT 1
+FROM users
+WHERE email = 'email'
+  AND password = 'password';
+
+-- Register Process
+INSERT INTO users(email, password, firstname, lastname)
+VALUES ('lang.s03@htlwienwest.at', 'hash', 'S', 'L');
+--> Login Queries
+
+-- Update User Data
+UPDATE users
+SET firstname='',
+    lastname=''
+WHERE email = 'lang.s03@htlwienwest.at';
+-- Update User Password
+UPDATE users
+SET password=''
+WHERE email = 'lang.s03@htlwienwest.at';
+
+-- Delete User
+DELETE
+FROM files
+WHERE id = (SELECT DISTINCT id
+            FROM users
+                     JOIN user_data USING (email)
+                     JOIN files ON id = data_id
+            WHERE email = 'email');
+--
+DELETE
+FROM users
+WHERE email = 'email';
+
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- -- # Files
+
+-- Reload User Files
+SELECT id, name, path, synonym_path, upload_date, admin
+FROM users
+         JOIN user_data USING (email)
+         JOIN files ON id = data_id
+WHERE email = 'lang.s03@htlwienwest.at';
+
+-- Insert User Files
+INSERT INTO files(id, name, path, synonym_path, upload_date)
+VALUES (2, 'File', 'user/folder/filename', 'synonym', now());
+--
+INSERT INTO user_data(email, data_id, admin)
+VALUES ('palatin.d02@htlwienwest.at', 2, true);
