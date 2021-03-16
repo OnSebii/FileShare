a00a87d01cb2813fc9570805b5d279d5de7f4881
OnSebii <lang.sebastian.006@gmail.com>
2021-03-16 09:22:40 +0100

    Update fsfunctions.js

--- functions/fsfunctions.js
+++ functions/fsfunctions.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+//TODO Console.error durch error function ersetzen
+
 function writeFile(email, file) {
   const dir = path.join(__dirname, email);
   try {
@@ -10,11 +12,35 @@ function writeFile(email, file) {
   }
 }
 
+function writeFileAnonym(file) {
+  const dir = path.join(__dirname, 'anonym');
+  try {
+    if (fs.existsSync(dir) == false) fs.mkdirSync(dir);
+    //TODO File speichern
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function getFile(email, fileName) {
+  const dir = path.join(__dirname, email);
+  try {
+    if (fs.existsSync(dir) == false) {
+      return { code: 404, message: 'Files from mail not found' };
+    }
+    return {
+      code: 200,
+      message: 'Files',
+    };
+  } catch (err) {
+    console.error(err);
+  }
+}
+
 const time = 1000 * 3600 * 24 * 7;
 setInterval(function () {
   console.log('1');
 }, time);
-console.log(time);
 
 console.log('2');
 
