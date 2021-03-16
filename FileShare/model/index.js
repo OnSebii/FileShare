8dd963b2f27e7828af7434f21675f056f1936f30
OnSebii <lang.sebastian.006@gmail.com>
2021-03-16 10:27:28 +0100

    Update index.js

--- FileShare/model/index.js
+++ FileShare/model/index.js
@@ -1,11 +1,37 @@
 const db = require('../db');
 
-async function writeFileAnonym(file) {
-  const dir = path.join(__dirname, "anonym");
+function writeFile(email, file) {
+  const dir = path.join(__dirname, email);
   try {
     if (fs.existsSync(dir) == false) fs.mkdirSync(dir);
+    //TODO File speichern
   } catch (err) {
     console.log(err);
+  }
+}
+
+function writeFileAnonym(file) {
+  const dir = path.join(__dirname, 'anonym');
+  try {
+    if (fs.existsSync(dir) == false) fs.mkdirSync(dir);
+    //TODO File speichern
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function getFile(email, fileName) {
+  const dir = path.join(__dirname, email);
+  try {
+    if (fs.existsSync(dir) == false) {
+      return { code: 404, message: 'Files from mail not found' };
+    }
+    return {
+      code: 200,
+      message: 'Files', //TODO file
+    };
+  } catch (err) {
+    console.error(err);
   }
 }
 
