7c00962e9ef610d9a88907f78908dc21bb1a8141
domi555 <66876360+domi555@users.noreply.github.com>
2021-03-23 13:01:45 +0100

    Update

---
+++ FileShare Vue/fileshare-app/src/views/Home.vue
@@ -0,0 +1,275 @@
+<template>
+  <div>
+    <!-- Nav Bar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+      <a class="navbar-brand" href="#">Domain.name</a>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Help</a>
+          </li>
+        </ul>
+        <span class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </span>
+      </div>
+    </nav>
+    <!-- Nav Bar ende -->
+
+    <!-- Main Container -->
+    <div class="container">
+      <div class="text-center landing-title-body">
+        <h1 class="glitch">File Share</h1>
+        <!-- TODO: Subtext hinzufügen. -->
+        <h5>Subtext</h5>
+      </div>
+      <div class="text-center">
+        <a class="" id="subtitle"></a>
+      </div>
+
+      <hr />
+      <h3 class="text-center">About</h3>
+      <!-- FAQ_ -->
+      <div>
+        <p><strong>How is (File Share) Unique?</strong></p>
+        <!-- TODO: Frage beantworten -->
+        <a>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet.
+        </a>
+      </div>
+
+      <hr />
+
+      <!-- Features Tab  jeweils mit den eigenen Cards-->
+      <h3 class="text-center">Features</h3>
+      <br />
+      <div class="row text-center">
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <i class="material-icons mi">done_all</i>
+              <h4 class="card-title">Easy to use</h4>
+              <span class="card-text"
+                >We are easy to use. Just upload your file and share it with
+                your friends.</span
+              >
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <i class="material-icons mi">accessibility</i>
+              <h4 class="card-title">Made for everyone</h4>
+              <span class="card-text"
+                >Made for everyone! This is our slogan.</span
+              >
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <i class="material-icons mi">attach_money</i>
+              <h4 class="card-title">No payments</h4>
+              <span class="card-text"
+                >We say no to payments. We're completely free to use.</span
+              >
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <i class="material-icons mi">https</i>
+              <h4 class="card-title">Security</h4>
+              <span class="card-text">We are full HTTPS protected.</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <i class="material-icons mi">hourglass_empty</i>
+              <h4 class="card-title">Auto delete</h4>
+              <span class="card-text"
+                >(File Share) will automatically delete all files which get
+                uploaded anonymously.</span
+              >
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <i class="material-icons mi">folder_open</i>
+              <h4 class="card-title">Open Source</h4>
+              <span class="card-text">
+                <!-- TODO: Github Link mit eigener Domain ersetzen. -->
+                Our whole project is open source. You can find the source code
+                on
+                <a
+                  href="https://github.com/OnSebii/FileShare"
+                  target="_blank"
+                  style="color: #2db194 !important"
+                  >GitHub</a
+                >
+                .
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Features Tab  jeweils mit den eigenen Cards ende-->
+
+      <hr />
+      <h3 class="text-center m-3 mb-4">How to use it?</h3>
+      <div>
+        <div class="row">
+          <div class="col-6">
+            <h4>Lorem Ipsum is simply dummy text of the printing</h4>
+            <a>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </a>
+          </div>
+          <div class="col-6 mb-5">
+            <img
+              src="../assets/img/300.png"
+              class="rounded mx-auto d-block"
+              alt="Placeholder image"
+            />
+          </div>
+
+          <div class="col-6">
+            <img
+              src="../assets/img/300.png"
+              class="rounded mx-auto d-block"
+              alt="Placeholder image"
+            />
+          </div>
+          <div class="col-6">
+            <h4>Lorem Ipsum is simply dummy text of the printing</h4>
+            <a>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <!-- Footer -->
+      <div class="landing-footer-body footer-bottom container">
+        <div class="float-left">
+          <span class="footer-title"
+            >Copyright © 2021&ensp;|&ensp;File Share&ensp;|&ensp;All rights
+            reserved</span
+          >
+          <br />
+          <!-- TODO: Mail hinzufügen sobald wir eine Domain haben -->
+          <a>Email: admin@domain.name</a>
+        </div>
+        <div class="float-right">
+          <span class="footer-title">Links</span>
+          <br />
+          <a target="_blank" href="https://material.io/resources/icons/"
+            >Icons</a
+          >
+          <a href="help/index.html">Terms Of Service</a>
+          <a href="help/index.html">Privacy Policy</a>
+        </div>
+      </div>
+    </div>
+    <!-- Footer ende -->
+    <!-- Main Container alles ist drinnen ende -->
+  </div>
+</template>
+
+<style scoped>
+body {
+  background-color: #171717;
+}
+
+hr {
+  width: 250px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  border: 0;
+  height: 0;
+  border-top: 1px solid #2db194;
+  box-sizing: content-box;
+}
+
+a {
+  color: #91979a !important;
+}
+
+span {
+  color: #91979a !important;
+}
+
+/* Damit die Klasse Card Transparent ist */
+.card {
+  background-color: transparent !important;
+  border: transparent !important;
+}
+
+.landing-footer-body {
+  /* position: fixed; */
+  height: 100px;
+  bottom: 0;
+  width: 100%;
+  margin-top: 70px;
+}
+
+.landing-footer-body a {
+  margin-right: 5px;
+  font-size: 13px;
+}
+
+.footer-title {
+  font-size: 14px;
+  color: #fff !important;
+}
+
+.mi {
+  color: #2db194;
+  font-size: 64px !important;
+}
+
+.landing-title-body {
+  padding-top: 5%;
+  text-align: center;
+}
+</style>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
---
+++ FileShare Vue/fileshare-app/src/views/Login.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="screen" class="d-flex flex-column justify-content-center align-items-center">
+    <form class="d-flex flex-column align-items-center p-4 bg-dark rounded">
+      <p class="icon mt-2 mb-1"><i class="fas fa-users"></i></p>
+      <h4 class="font-weight-normal">Please sign in</h4>
+
+      <label for="inputEmail" class="mt-3 w-100">Email address</label>
+      <input
+        type="email"
+        id="inputEmail"
+        class="form-control input-field"
+        placeholder="Email address"
+        required
+        autofocus
+      />
+
+      <label for="inputPassword" class="mt-3 w-100">Password</label>
+      <input
+        type="password"
+        id="inputPassword"
+        class="form-control input-field"
+        placeholder="Password"
+        required
+      />
+      <div class="mt-2">
+        <button class="btn mt-3 highlighted-button mr-1" type="submit">Login</button>
+        <button class="btn background-button mt-3" type="submit">
+          Register
+        </button>
+      </div>
+
+      <p class="mt-3 mb-3 text-muted">&copy; 2021 Fileshare</p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+};
+</script>
+
+<style scoped>
+#screen {
+  height: 100vh;
+}
+form {
+  width: 100%;
+  max-width: 330px;
+  margin-bottom: 20vh;
+}
+
+.icon {
+  font-size: 4.6em;
+  color: #2aa58a;
+}
+
+/* Dark Mode Input */
+.input-field {
+  color: #fff;
+  background-color: #444444;
+  border: 1px solid #686868;
+}
+.input-field:focus {
+  color: #fff;
+  background-color: #505050;
+}
+.input-field:active {
+  color: #fff;
+  background-color: #686868;
+}
+.input-field::placeholder {
+  color: #fff;
+}
+
+/* Customized Button Colors */
+.highlighted-button {
+  background-color: #2aa58a;
+}
+.highlighted-button:hover {
+  background-color: #268a74;
+}
+.background-button {
+  background-color: #585858;
+}
+.background-button:hover {
+  background-color: #494949;
+}
+</style>
