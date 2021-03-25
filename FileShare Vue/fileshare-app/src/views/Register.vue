887ae63e31671b879290b1d1259ee23ebd294882
OnSebii <lang.sebastian.006@gmail.com>
2021-03-25 10:02:47 +0100

    Added register side

--- FileShare Vue/fileshare-app/src/views/Register.vue
+++ FileShare Vue/fileshare-app/src/views/Register.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="screen" class="d-flex flex-column justify-content-center align-items-center">
     <form class="d-flex flex-column align-items-center p-4 bg-dark rounded">
-      <p class="icon mt-2 mb-1"><i class="fas fa-users"></i></p>
-      <h4 class="font-weight-normal">Please sign in</h4>
+      <p class="icon mt-2 mb-1"><i class="fas fa-user-plus"></i></p>
+      <h4 class="font-weight-normal">Create an account</h4>
+
+      <label for="inputFirst" class="mt-3 w-100">First name</label>
+      <input type="text" id="inputFirst" class="form-control input-field" placeholder="First name" required autofocus />
+
+      <label for="inputLast" class="mt-3 w-100">Last name</label>
+      <input type="text" id="inputLast" class="form-control input-field" placeholder="Last name" required autofocus />
 
       <label for="inputEmail" class="mt-3 w-100">Email address</label>
-      <input type="email" id="inputEmail" class="form-control input-field" placeholder="Email address" required autofocus />
+      <input type="text" id="inputEmail" class="form-control input-field" placeholder="Email address" required autofocus />
 
       <label for="inputPassword" class="mt-3 w-100">Password</label>
       <input type="password" id="inputPassword" class="form-control input-field" placeholder="Password" required />
       <div class="mt-2">
-        <button class="btn mt-3 highlighted-button mr-1" type="submit">Login</button>
-        <button class="btn background-button mt-3" type="submit">
+        <button class="btn mt-3 highlighted-button" type="submit">
           Register
         </button>
+        <button class="btn background-button mt-3" type="submit">Login</button>
       </div>
 
       <p class="mt-3 mb-3 text-muted">&copy; 2021 Fileshare</p>
@@ -22,9 +28,7 @@
 </template>
 
 <script>
-export default {
-  name: 'Login',
-};
+export default {};
 </script>
 
 <style scoped>
@@ -33,12 +37,12 @@ export default {
 }
 form {
   width: 100%;
-  max-width: 330px;
-  margin-bottom: 20vh;
+  max-width: 360px;
+  margin-bottom: 10vh;
 }
 
 .icon {
-  font-size: 4.6em;
+  font-size: 3.5em;
   color: #2aa58a;
 }
 
