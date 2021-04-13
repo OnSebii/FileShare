37f3750cda243f2aab5c0b1bbb2537705d6a4a08
OnSebii <lang.sebastian.006@gmail.com>
2021-04-13 10:00:43 +0200

    Update

--- FileShare SERVER/routes/index.js
+++ FileShare SERVER/routes/index.js
@@ -1,27 +1,43 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 const router = express.Router();
-const {registerUser} = require('../model');
+const { registerUser } = require('../model');
 
 router.post(
   '/upload',
   asyncHandler(async (req, res) => {
-    const result = await (req.body);
+    // const result = await req.body;
+    // res.status(result.code).json(result);
+    console.log(req.files.upload);
+  }),
+);
+
+router.get(
+  '/register',
+  asyncHandler(async (req, res) => {
+    // TODO Register page
+    //res.status(result.code).send();
+  }),
+);
+
+router.post(
+  '/register',
+  asyncHandler(async (req, res) => {
+    // TODO Hashing function
+    //const password = bcrypt(req.body.password);
+    const result = await registerUser(req.body.email, passwort, req.body.firstname, req.body.lastname);
     res.status(result.code).json(result);
   }),
 );
 
-router.get("/register", asyncHandler(async (req, res) => {
-  // TODO Register page
-  //res.status(result.code).send();
-}));
-
-router.post("/register", asyncHandler(async (req, res) => {
-  // TODO Hashing function
-  //const password = bcrypt(req.body.password);
-  const result = await registerUser(req.body.email, passwort, req.body.firstname, req.body.lastname);
-  res.status(result.code).json(result);
-}));
+router.get(
+  '/:path',
+  asyncHandler(async (req, res) => {
+    // TODO path zum File zurückgeben
+    // Wenn das File nicht vorhanden ist: 404
+    //res.status(result.code).send();
+  }),
+);
 
 // router.get(
 //   '/employees',
