d07cd13d9b73c63f67a0e50ee7b4cbffa1e2f77b
OnSebii <lang.sebastian.006@gmail.com>
2021-05-18 11:54:40 +0200

    Create deleteFile.js

---
+++ FileShare SERVER/model/deleteFile.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const fs = require('fs');
+
+function deleteFile(filePath, name) {
+  // path => upload/...
+  // name => upload/.../name.xyz
+  const dir = path.join(__dirname, '../upload/', filePath, name);
+  fs.unlinkSync(dir);
+}
+
+// Example deleteFile('anon', 'test.png');
+
+// Route delete/file aufrufen
