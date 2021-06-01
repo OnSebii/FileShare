b8b911f095c34d144adb3e84a49a791f76bb0198
domi555 <66876360+domi555@users.noreply.github.com>
2021-06-01 11:07:00 +0200

    Update

--- FileShare Vue/fileshare-app/src/views/Dashboard.vue
+++ FileShare Vue/fileshare-app/src/views/Dashboard.vue
@@ -114,7 +114,10 @@
             <div class="card-header d-flex justify-content-between align-items-center px-3 py-2">
               <p class="m-0 custom-title">{{ file.name }}</p>
               <div class="d-flex justify-content-end align-items-center">
-                <button class="btn btn-sm mr-2 custom-front-button" @click="download(file.path)">
+                <button
+                  class="btn btn-sm mr-2 custom-front-button"
+                  @click="$router.push(`/down?user=${user.email}&file=${file.path}`)"
+                >
                   Download <i class="fas fa-copy ml-1"></i>
                 </button>
                 <button
@@ -172,7 +175,10 @@
             <div class="card-header d-flex justify-content-between align-items-center px-3 py-2">
               <p class="m-0 custom-title">{{ file.name }}</p>
               <div class="d-flex justify-content-end align-items-center">
-                <button class="btn btn-sm custom-front-button" @click="download(file.path)">
+                <button
+                  class="btn btn-sm custom-front-button"
+                  @click="$router.push(`/down?user=${user.email}&file=${file.path}`)"
+                >
                   Download <i class="fas fa-copy ml-1"></i>
                 </button>
               </div>
@@ -428,10 +434,6 @@ export default {
         let temp = new Date().getTime() - start.getTime();
         return (temp / margin) * 100;
       }
-    },
-    download(path) {
-      // TODO
-      console.log(path);
     },
     async openShareModal(file) {
       try {
